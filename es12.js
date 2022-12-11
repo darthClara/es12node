d9f9f73f18381530ee0439a7c3fcbe387bb50bf9
Clara Schillaci <dhdjjk>
2022-12-11 12:42:29 +0100

    es12

---
+++ es12.js
@@ -0,0 +1,24 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+  }
+
+  async function getResult() {
+    const tina = await luckyDraw("Tina");
+    console.log(tina);
+    const jorge = await luckyDraw("Jorge");
+    console.log(jorge);
+    const julien = await luckyDraw("Julien");
+    console.log(julien);
+  }
+
+  getResult()
